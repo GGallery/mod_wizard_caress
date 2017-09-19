accaf6475462d556d848fa22b4e5ec4c4931ebdf
GGallery <antonio@ggallery.it>
2017-09-19 15:40:35 +0200

    aggiunto alert e glossario

--- mod_wizard.php
+++ mod_wizard.php
@@ -2,6 +2,13 @@
 
 defined('_JEXEC') or die;
 
+$session = JFactory::getSession();
+$step = $session->get('step');
+
+$app = JFactory::getApplication();
+$menu   = $app->getMenu()->getActive();
+
+$alert = (($menu->component != 'com_wizard') && $step!= ''  ) ? 1 : 0;
 
 $doc = JFactory::getDocument();
 $doc->addStyleSheet(JURI::root(true) ."/modules/mod_wizard/mod_wizard.css");
