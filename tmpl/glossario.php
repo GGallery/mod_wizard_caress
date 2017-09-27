26b131e080af6848d0835bb27f36e6b36d495422
GGallery <antonio@ggallery.it>
2017-09-27 15:39:00 +0200

    aggiunto pulsante glossario come popup

--- tmpl/glossario.php
+++ tmpl/glossario.php
@@ -1,6 +1,31 @@
 <div id="glossary">
-    <a alt="Glossary" title="Glossary"  class="btn btn-success btn-large btn-glossary" href="<?php echo $params->get('url_glossario'); ?>">
+    <a alt="Glossary" title="Glossary"  class="btn btn-success btn-large btn-glossary"   data-toggle="modal" href="#myModal">
         <span class="fa fa-book"></span>
         Glossary</a>
 </div>
 
+
+<!-- Modal HTML -->
+
+<div id="myModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Glossary</h4>
+            </div>
+
+            <div class="modal-body">
+
+                <?php echo $params->get('glossario'); ?>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
